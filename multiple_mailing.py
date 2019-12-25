bb9605cb1611061d63dfa4160e130f72fcdf680b
adarshbhagat <59194987+adarshbhagat@users.noreply.github.com>
2019-12-25 13:53:03 +0530

    Update multiple_mailing.py

--- multiple_mailing.py
+++ multiple_mailing.py
@@ -7,11 +7,11 @@ for i in range(1805035,1805045):
     email.append(j+"@kiit.ac.in")
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
-server.login("adarsh.nwd@gmail.com","transversal")
+server.login("your_mail_id@gmail.com","your_password")
 msg = "One day left for the orientation!!! Grab your opportunity to get glimpses of the courses and exciting projects that we will be conducting in the upcoming weeks...Do register in the link https://forms.gle/m4aY8PHbmcrPi5R3A before it gets housefull"
 subject = "KIIT ROBOTICS SOCIETY"
 body="subject:{}\n\n{}".format(subject,msg)
 for x in email:
-    server.sendmail("adarsh.nwd@gmail.com",x,body)
+    server.sendmail("your_mail_id@gmail.com",x,body)
     print(x+"  Done")
 server.quit()
